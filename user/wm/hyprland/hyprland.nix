222e8b4dd7fd02c28ba60f4edbf0b5878463f24f
Matteo Cavestri <matteo.cavestri@protonmail.ch>
2024-08-24 07:32:30 +0200

    edited key-layout

--- user/wm/hyprland/hyprland.nix
+++ user/wm/hyprland/hyprland.nix
@@ -55,7 +55,7 @@ in {
         # $menu = rofi -show drun
         $menu = fuzzel
         $editor = kitty -e nvim
-            
+          
         env = XDG_CURRENT_DESKTOP,Hyprland
         env = XDG_SESSION_TYPE,wayland
         env = XDG_SESSION_DESKTOP,Hyprland
@@ -78,7 +78,7 @@ in {
         exec-once = hypridle
         exec-once = sleep 5 && libinput-gestures
         #exec-once = pypr
-        
+              
         # General Settings
           general { 
             gaps_in = 7
@@ -118,7 +118,7 @@ in {
 
         # Input Settings
           input {
-            kb_layout = us
+            kb_layout = it
             kb_variant =
             kb_model =
             kb_options =
