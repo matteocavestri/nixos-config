c1353d0580fab03d4112be869a4536d7e79de652
Matteo Cavestri <matteo.cavestri@protonmail.ch>
2024-09-01 19:35:40 +0200

    edited wireless config

--- system/hardware/iwd.nix
+++ system/hardware/iwd.nix
@@ -5,7 +5,10 @@
 }: {
   networking = {
     hostName = systemSettings.hostname; # Define your hostname.
-    networkmanager.enable = true; # Easiest to use and most distros use this by default.
+    networkmanager = {
+      enable = true; # Easiest to use and most distros use this by default.
+      wifi.backend = "iwd";
+    };
     wireless.iwd = {
       enable = true;
       settings.General.EnableNetworkConfiguration = true;
--- system/hardware/wpa_supplicant.nix
+++ system/hardware/wpa_supplicant.nix
@@ -6,7 +6,10 @@
   # Networking Configuration
   networking = {
     hostName = systemSettings.hostname;
-    networkmanager.enable = true; # Easiest to use and most distros use this by default.
+    networkmanager = {
+      enable = true; # Easiest to use and most distros use this by default.
+      wifi.backend = "wpa_supplicant";
+    };
     firewall.checkReversePath = false;
   };
   users.users.${userSettings.username}.extraGroups = ["networkmanager"];
