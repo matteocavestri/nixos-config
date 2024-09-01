e62e5027e93c7094b4b1e7daeb3b2f775a280296
Matteo Cavestri <matteo.cavestri@protonmail.ch>
2024-09-01 19:13:56 +0200

    updated, added kitty

--- profiles/minimal-gui/home.nix
+++ profiles/minimal-gui/home.nix
@@ -1,6 +1,7 @@
 {userSettings, ...}: {
   imports = [
     ../minimal/home.nix
+    ../../user/pkgs/terminal/kitty.nix
     (./. + "../../../user/pkgs/browsers" + ("/" + userSettings.browser) + ".nix") # Browser definition
     (./. + "../../../user/wm" + ("/" + userSettings.wm + "/" + userSettings.wm) + ".nix") # Window manager import
   ];
