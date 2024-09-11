50898071a8fd86349dcc94c67ced275db9cedd04
Matteo Cavestri <matteo.cavestri@protonmail.ch>
2024-09-11 18:37:52 +0200

    fix xorg

--- nixosModules/services/displaymanager.nix
+++ nixosModules/services/displaymanager.nix
@@ -26,7 +26,7 @@ in {
   config = {
     services = {
       xserver = {
-        enable = lib.mkIf (config.system.services.wayland.enable || config.system.services.x11.enable) true;
+        enable = lib.mkIf (config.system.services.wayland.enable || config.system.services.xorg.enable) true;
         displayManager = {
           # Lightdm main configuration
           lightdm = lib.mkIf config.system.services.displaymanager.lightdm.enable {
