ed652e9b9b92d00ec2b4ef50b89bb8c5836347a3
Matteo Cavestri <matteo.cavestri@protonmail.ch>
2024-09-12 14:26:37 +0200

    end gnome pkgs

--- nixosModules/desktop/gnome.nix
+++ nixosModules/desktop/gnome.nix
@@ -1,168 +1,6 @@
-# {pkgs, ...}: {
-#   # Depencency services
-#   system.services.wayland.enable = true;
-#   hardware.pulseaudio.enable = false;
-#   # Enable Gnome desktop environment and gdm display manager
-#   services = {
-#     xserver = {
-#       displayManager.gdm.enable = true;
-#       desktopManager.gnome.enable = true;
-#     };
-#     udev.packages = with pkgs; [gnome.gnome-settings-daemon];
-#   };
-#   # Ignore gnome default packages
-#   environment = {
-#     gnome.excludePackages = with pkgs; [
-#       gnome-photos
-#       gnome-tour
-#       xterm
-#       gedit
-#       snapshot
-#       baobab
-#       tracker
-#       tracker-miners
-#       gnome-connections
-#       gnome-text-editor
-#       gnome-console
-#       gnome-builder
-#       loupe
-#       (
-#         if (lib.versionOlder (lib.versions.majorMinor lib.version) "24.11")
-#         then gnome.gnome-calendar
-#         else gnome-calendar
-#       )
-#       (
-#         if (lib.versionOlder (lib.versions.majorMinor lib.version) "24.11")
-#         then gnome.nautilus
-#         else nautilus
-#       )
-#       (
-#         if (lib.versionOlder (lib.versions.majorMinor lib.version) "24.11")
-#         then gnome.gnome-calculator
-#         else gnome-calculator
-#       )
-#       (
-#         if (lib.versionOlder (lib.versions.majorMinor lib.version) "24.11")
-#         then gnome.gnome-music
-#         else gnome-music
-#       )
-#       (
-#         if (lib.versionOlder (lib.versions.majorMinor lib.version) "24.11")
-#         then gnome.gnome-font-viewer
-#         else gnome-font-viewer
-#       )
-#       (
-#         if (lib.versionOlder (lib.versions.majorMinor lib.version) "24.11")
-#         then gnome.gnome-maps
-#         else gnome-maps
-#       )
-#       (
-#         if (lib.versionOlder (lib.versions.majorMinor lib.version) "24.11")
-#         then gnome.file-roller
-#         else file-roller
-#       )
-#       (
-#         if (lib.versionOlder (lib.versions.majorMinor lib.version) "24.11")
-#         then gnome.gnome-contacts
-#         else gnome-contacts
-#       )
-#       (
-#         if (lib.versionOlder (lib.versions.majorMinor lib.version) "24.11")
-#         then gnome.gnome-disk-utility
-#         else gnome-disk-utility
-#       )
-#       (
-#         if (lib.versionOlder (lib.versions.majorMinor lib.version) "24.11")
-#         then gnome.gnome-weather
-#         else gnome-weather
-#       )
-#       (
-#         if (lib.versionOlder (lib.versions.majorMinor lib.version) "24.11")
-#         then gnome.gnome-clocks
-#         else gnome-clocks
-#       )
-#       (
-#         if (lib.versionOlder (lib.versions.majorMinor lib.version) "24.11")
-#         then gnome.geary
-#         else geary
-#       )
-#       (
-#         if (lib.versionOlder (lib.versions.majorMinor lib.version) "24.11")
-#         then gnome.gnome-system-monitor
-#         else gnome-system-monitor
-#       )
-#       (
-#         if (lib.versionOlder (lib.versions.majorMinor lib.version) "24.11")
-#         then gnome.gnome-logs
-#         else gnome-logs
-#       )
-#       (
-#         if (lib.versionOlder (lib.versions.majorMinor lib.version) "24.11")
-#         then gnome.dconf-editor
-#         else dconf-editor
-#       )
-#       (
-#         if (lib.versionOlder (lib.versions.majorMinor lib.version) "24.11")
-#         then gnome.simple-scan
-#         else simple-scan
-#       )
-#       (
-#         if (lib.versionOlder (lib.versions.majorMinor lib.version) "24.11")
-#         then gnome.devhelp
-#         else devhelp
-#       )
-#       (
-#         if (lib.versionOlder (lib.versions.majorMinor lib.version) "24.11")
-#         then gnome.cheese
-#         else cheese
-#       )
-#       (
-#         if (lib.versionOlder (lib.versions.majorMinor lib.version) "24.11")
-#         then gnome.epiphany
-#         else epiphany
-#       )
-#       (
-#         if (lib.versionOlder (lib.versions.majorMinor lib.version) "24.11")
-#         then gnome.evince
-#         else evince
-#       )
-#       (
-#         if (lib.versionOlder (lib.versions.majorMinor lib.version) "24.11")
-#         then gnome.totem
-#         else totem
-#       )
-#       (
-#         if (lib.versionOlder (lib.versions.majorMinor lib.version) "24.11")
-#         then gnome.tali
-#         else tali
-#       )
-#       (
-#         if (lib.versionOlder (lib.versions.majorMinor lib.version) "24.11")
-#         then gnome.iagno
-#         else iagno
-#       )
-#       (
-#         if (lib.versionOlder (lib.versions.majorMinor lib.version) "24.11")
-#         then gnome.hitori
-#         else hitori
-#       )
-#       (
-#         if (lib.versionOlder (lib.versions.majorMinor lib.version) "24.11")
-#         then gnome.atomix
-#         else atomix
-#       )
-#       (
-#         if (lib.versionOlder (lib.versions.majorMinor lib.version) "24.11")
-#         then gnome.yelp
-#         else yelp
-#       )
-#     ];
-#   };
-# }
 {
   pkgs,
   lib,
-  config,
   ...
 }: {
   # Depencency services
@@ -175,13 +13,18 @@
       displayManager.gdm.enable = true;
       desktopManager.gnome.enable = true;
     };
-    udev.packages = with pkgs; [gnome.gnome-settings-daemon];
+    udev.packages = with pkgs; [
+      (
+        if (lib.versionOlder (lib.versions.majorMinor lib.version) "24.11")
+        then gnome.gnome-settings-daemon
+        else gnome-settings-daemon
+      )
+    ];
   };
 
   # Ignore gnome default packages
   environment = {
     gnome.excludePackages = let
-      # Lista dei pacchetti che possono avere il prefisso "gnome."
       packagesWithPrefix = [
         "gnome-calendar"
         "nautilus"
@@ -211,13 +54,12 @@
         "yelp"
       ];
 
-      # Funzione per aggiungere il prefisso "gnome." se necessario
+      # Filter packages per nixos version (for > 24.05)
       gnomePackage = pname:
         if lib.versionOlder (lib.versions.majorMinor lib.version) "24.11"
         then pkgs.gnome."${pname}"
         else pkgs."${pname}";
     in
-      # Applicazione del ciclo per gestire tutti i pacchetti
       (with pkgs; [
         gnome-photos
         gnome-tour
