d11d6f62212c65d7ebfe175eafdf12c03104b558
Matteo Cavestri <matteo.cavestri@protonmail.ch>
2024-09-25 17:28:38 +0200

    added intel driver

--- profile/host/k3s-0/config.nix
+++ profile/host/k3s-0/config.nix
@@ -7,5 +7,14 @@
       zram.enable = true;
       unfree.enable = true;
     };
+    hardware = {
+      gpu.intel = {
+        enable = true;
+        support32 = true;
+        opencl = true;
+        monitoring = true;
+        newgpu5 = true;
+      };
+    };
   };
 }
