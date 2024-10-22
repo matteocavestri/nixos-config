85ff78b20aa25592f9f1f96c45889f6dd95d5d20
Matteo Cavestri <matteo.cavestri@protonmail.ch>
2024-10-22 18:51:22 +0200

    set librewolf to macmini (torBrowser is x86 only)

--- profile/host/macmini-m1/home.nix
+++ profile/host/macmini-m1/home.nix
@@ -1,8 +1,10 @@
-{
+{lib, ...}: {
   user.packages = {
     minecraft.enable = true;
     mangohud.enable = true;
     gamemode.enable = true;
     kube.enable = true;
+    librewolf.enable = true;
+    torBrowser.enable = lib.mkForce false;
   };
 }
