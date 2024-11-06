9d948d5150e2d4ac5d0d8c6011d343d7572b4fed
Matteo Cavestri <matteo.cavestri@protonmail.ch>
2024-11-06 16:20:31 +0100

    added initial flake

--- flake.nix
+++ flake.nix
@@ -1,4 +1,45 @@
 {
+  outputs = {
+    nixpkgs,
+    nixpkgs-unstable,
+    self,
+    ...
+  } @ inputs: let
+    # -------------------------------------------------------------------
+    inherit (nixpkgs) lib;
+    pkgs = import nixpkgs;
+    pkgs-unstable = import nixpkgs-unstable;
+  in {
+    # -------------------- NixOS Configuration --------------------------
+    nixosConfigurations = {
+      desktop = lib.nixosSystem {
+        modules = [
+          ./profiles/desktop.nix
+        ];
+        specialArgs = {
+          inherit inputs;
+          inherit pkgs-unstable;
+        };
+      };
+    };
+  };
+
+  # -------------------Inputs -----------------------------------------
   inputs = {
+    # ------------------ NixPkgs ----------------------------------------
+    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    # ------------------ Firefox Addons ----------------------------------
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # ------------------ Nevica ------------------------------------------
+    nevica = {
+      url = "github:matteocavestri/nevica";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
   };
 }
